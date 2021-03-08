de71b9bb60306d06b526e5ce336643bc6ffec344
Tail <12040761+tailpiece@users.noreply.github.com>
2021-03-08 23:40:10 +0900

    main ul以外も対応させる

--- docs/content/content.js
+++ docs/content/content.js
@@ -13,18 +13,18 @@ chrome.runtime.onMessage.addListener(function(msg) {
   if (msg.gradient && msg.gradient !== '') {
     if (msg.gradient !== "remove") {
       GradientMaps.applyGradientMap(target, msg.gradient);
-      if (main && layers && !cssId) {
+      if (main.length > 0 && layers && !cssId) {
         const filterId = target.getAttribute('data-gradientmap-filter');
         const fragment = document.createDocumentFragment();
         const style = document.createElement('style');
         style.id = coloredGlassesId;
-        style.innerText  = '#layers ul { filter: url(#' + filterId + '); }';
+        style.innerText  = '#layers ul, #layers > div > div > div > div > div > div > div { filter: url(#' + filterId + '); }';
         fragment.appendChild(style);
-        main.appendChild(fragment);
+        main[0].appendChild(fragment);
       }
     } else {
       GradientMaps.removeGradientMap(target);
-      if (main && layers && cssId) {
+      if (main.length > 0 && layers && cssId) {
         cssId.remove();
       }
     }
